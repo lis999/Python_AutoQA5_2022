45293e0297cd8eee70063b7d9352545ecca7078b
Sergey Li <li.sergey@yahoo.com>
2022-09-29 09:44:45 -0700

    trying to update with poetry

--- Homework QA5/Lesson 5 (OOP, Git, GitHub)/5.1. Class creation.py
+++ Homework QA5/Lesson 5 (OOP, Git, GitHub)/5.1. Class creation.py
@@ -4,3 +4,42 @@
     нужно использовать методы get и set
 """
 
+class Investments:
+    def __int__(self, ticker, market):
+        self.ticker = ticker
+        self.market = market
+
+    def market_price(self):
+        return f'Current Market Price: {self}'
+
+
+class Stocks(Investments):
+    def __int__(self, ticker, market, dividends):
+        super().__int__(ticker, market)
+        self.dividends = dividends
+
+    def dividend_premium(self):
+        return f'Forward Dividend Yeld: {self}%'
+
+
+
+class Bond(Investments):
+    def coupons(self):
+        return f'Dividend (fixed) Yeld: {self}%'
+
+
+class ETF(Stocks):
+    def index(self):
+        return f'Included into {self}'
+
+intel = Stocks()
+
+sp500 = ETF('VOO', 'US')
+
+vanguard = Bond('MAXXD', 'GLOBAL')
+
+print(intel.market_price(35))
+
+
+
+
